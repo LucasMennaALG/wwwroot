8af0a3e1ee80343fbb91ec1153c128d9b9d8f754
LucasMennaALG <106624728+LucasMennaALG@users.noreply.github.com>
2022-07-12 16:45:04 -0300

    Add files via upload

---
+++ jsinterop/wcangular.js
@@ -0,0 +1,24 @@
+﻿var interopJS = interopJS || {}
+
+interopJS.angular = {
+    dotNet: null,
+
+    init: _ => {
+        var chart = document.querySelector('componente-angular-widget');
+        //chart.addEventListener("selected", function (eventData) {
+        //    console.log('### wc-pie-chart event fired!');
+        //    console.log(eventData);
+
+        //    dotNet.invokeMethodAsync("SetSelectedCountry", eventData.detail)
+        //        .then(data => {
+        //            console.log("### wc-pie-chart was sent event details to .NET.");
+        //        });
+        //});
+    },
+
+    register: dotNetReference => {
+        dotNet = dotNetReference;
+    }
+}
+
+window.interop = interopJS;
